40da5e9635eed2b5ad8795c5d58e8d08d0c90701
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2022-01-07 09:16:21 +0530

    Add files via upload

---
+++ Python/Sum of n Natural number.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  7 09:04:27 2022
+
+@author: Guruprasada
+"""
+
+num = int(input("Enter a number"))
+sum = (num * (num +1))/2
+print("Sum of the n natural number is  {}".format(sum))
