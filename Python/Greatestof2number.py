2c042e456dfe8d000d6ea8836f6e957ad2a5593d
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2022-01-13 18:39:53 +0530

    Py Coder

---
+++ Python/Greatestof2number.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 13 18:23:01 2022
+
+@author: Guruprasada
+"""
+a = int(input("enter a number: "))
+b = int(input("enter a number: "))
+
+    
+if a>b:
+    print("a is largest number")
+elif b>a:
+    print("b is largest number")
+else:
+    print("equal:")
+              
