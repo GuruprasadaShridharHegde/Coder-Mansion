e76acb45fdf5d91e23d86a7aed34d02e1afd1e05
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2022-01-20 10:46:17 +0530

    Add files via upload

---
+++ Python/SumOfDigits.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 20 09:16:03 2022
+
+@author: Guruprasada
+"""
+num=int(input("Enter the number: "))
+sum=0
+for i in range(1,num+1):
+    sum+= i
+print(sum)
