3909add4cc102b027e7b7c05a298d3eb8df0e3ab
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2022-01-24 10:02:37 +0530

    Add files via upload

---
+++ Python/LengthofString.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 09:03:26 2022
+
+@author: Guruprasada
+"""
+string = input("enter the string:  ")
+count = 0
+for i in string:
+    count+=1
+print(count)
