5c03bec9bc24de2f480394e8bce52854454e987a
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2022-01-26 17:52:33 +0530

    Add files via upload

---
+++ Python/ArraySort.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 26 17:05:29 2022
+
+@author: Guruprasada
+"""
+
+arr = [6,6,5,10,15,20,6]
+arr.sort()
+print(arr[0]) 
+
+
+"""myList = [1 , 5 , 6 , 8 , 10, 10, 1]
+myList = sorted(set(myList))
+print(myList)
+"""
