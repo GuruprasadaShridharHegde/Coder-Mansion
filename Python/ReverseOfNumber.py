166c1aab15f422ec557b61abb7d4df525f19cd7f
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2022-02-02 17:40:45 +0530

    Add files via upload

---
+++ Python/ReverseOfNumber.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  2 17:27:33 2022
+
+@author: Guruprasada
+"""
+## using slicing
+"""
+num = 1
+print(int(num)[::-1])
+"""
+## Using while loop
+num = int(input("Enter the Number: "))
+temp = num
+reverse = 0
+while num > 0:
+    remainder = num % 10
+    reverse = (reverse * 10) + remainder
+    num = num // 10
+
+print("The Given number is {} and Reverse is {}".format(temp, reverse))
+
+
