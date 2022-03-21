2f6a6cf952da8fc18d5aaa8657b26bf194232cfe
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2022-03-21 13:02:10 +0530

    Add files via upload

---
+++ Python/My general experiments.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 17 15:05:05 2022
+
+@author: Guruprasada
+"""
+# My experiments
+print(tuple('random'))
+print(list('random'))
+print(set('random'))
+print(" it's my birthday")
+print('she said "I am hungry" ')
+#####
+test = 'hello guru'
+print(test.index("l"))
+print(test[1:4])
+print(test[:3])
+print(test[3:])
+#####
+char = 'fantasticguru'
+print(char[0::2])
+name = 'HIGHlow'
+print(name.upper(),name.lower())
+######
+
+txt = ("welcome to the  jungle")
+
+x = txt.split()
+
+print(x)
+#######
+first = 2
+second = 2
+x = first + second
+print(x)
+#######
+import calendar
+year = 2021
+month = 8
+print(calendar.month(year,month))
+
+#######
+print('It\'s my birthday')
+print("there is a \t space")
+print("First line\nSecond line")
