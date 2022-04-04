467ff66eee46a82bc7f80cc17860da26d4ad2dc3
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2022-04-04 22:38:36 +0530

    Add files via upload

---
+++ Python/systemInfo.py
@@ -0,0 +1,16 @@
+import platform
+
+#Computer network name
+print(f"Computer network name: {platform.node()}")
+#Machine type
+print(f"Machine type: {platform.machine()}")
+#Processor type
+print(f"Processor type: {platform.processor()}")
+#Platform type
+print(f"Platform type: {platform.platform()}")
+#Operating system
+print(f"Operating system: {platform.system()}")
+#Operating system release
+print(f"Operating system release: {platform.release()}")
+#Operating system version
+print(f"Operating system version: {platform.version()}")
