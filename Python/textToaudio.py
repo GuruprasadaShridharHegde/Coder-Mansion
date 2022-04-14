1b0d5b9ba98d62ab5671d2a754a06a5f327a4aa0
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2022-04-14 18:53:41 +0530

    Add files via upload

---
+++ Python/textToaudio.py
@@ -0,0 +1,8 @@
+from gtts import gTTS
+from playsound import playsound
+audio = 'speech.mp3'
+language = 'en'
+sp = gTTS(text = "Hello everyone",
+ lang = language, slow=False)
+sp.save(audio)
+playsound(audio)
