771d8255cd06543e887999de6ac1d1cf5434fd76
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2022-05-14 19:27:25 +0530

    Add files via upload

---
+++ Python/emoji1.py
@@ -0,0 +1,6 @@
+import emoji
+print(emoji.emojize(":grinning_face:"))
+print(emoji.emojize(":rolling_on_the_floor_laughing:"))
+print(emoji.emojize(":face_with_tears_of_joy:"))
+print(emoji.emojize(":slightly_smiling_face:"))
+print(emoji.emojize(":smiling_face_with_halo:"))
