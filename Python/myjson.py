612c8254528e3cec3e1ea33ac5be922980821d2b
Guruprasada Shridhar Hegde <gshegde6@gmail.com>
2023-06-17 19:54:03 +0530

    Coder mansion active

--- Python/myjson.py
+++ Python/myjson.py
@@ -2,4 +2,5 @@ import json
 
 g = '{"name": "guru", "age": "24", "weight":"54"}'
 y = json.loads(g)
-print(y["age"],y["weight"])
+print(y["age"],y["weight"],y["name"])
+# write a program to validate json in python
---
+++ testing.py
@@ -0,0 +1,3 @@
+x = 5
+y = "john"
+print(f"its {x}", "it is", {y})
