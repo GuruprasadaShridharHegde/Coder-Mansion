7e34d84c146f62c7f6354ff062680c8606597fc3
Guruprasada Shridhar Hegde <gshegde6@gmail.com>
2023-07-06 21:05:19 +0530

    new code

--- testing.py
+++ testing.py
@@ -15,8 +15,41 @@ for i in range(10):
     print(i)
 '''
 # Below is for pass example
+'''
 for i in range(10):
     if i == 2:
         pass
-    
         print(i)
+'''
+import datetime
+# If elif remove my Dictionary
+def first():
+    print("calling first")
+def second():
+    print("callin Second")
+def third():
+    print("calling third")
+def default():
+    print("Calling default")
+
+var = (int(input("enter the value:  ")))
+start = datetime.datetime.now()
+
+funcs: dict = {0:first,
+               1:second,
+               2:third}
+final = funcs.get(var, default)
+final()
+'''
+if var == 0:
+    first()
+elif var == 1:
+    second()
+elif var == 2:
+    third()
+else:
+    default()
+end = datetime.datetime.now()
+calculate = (end-start)
+print(calculate)
+'''
