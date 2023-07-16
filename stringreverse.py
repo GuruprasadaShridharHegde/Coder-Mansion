a1715ec5600d7c3e1517b39c67676f9629e24a2c
Guruprasada Shridhar Hegde <gshegde6@gmail.com>
2023-07-16 17:55:12 +0530

    String reverse

---
+++ stringreverse.py
@@ -0,0 +1,16 @@
+# String reverse using for loop in python
+"""
+Enter a number: 12345
+Reverse of the number: 54321
+"""
+def reverse_string(input_string):
+    reversed_string = ''
+    for char in reversed(input_string):
+        reversed_string += char
+    return reversed_string
+
+# Example usage
+original_string = "Hello, World!"
+reversed_string = reverse_string(original_string)
+print(reversed_string)
+
